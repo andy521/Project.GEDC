e111007ee4eb0f38d472d2a47905a242d7a4760d
ppoffice <ppoffice_2008@163.com>
2016-05-12 22:22:10 +0800

    chore: docker compose file windows fix

--- lumen/www/app/Http/Controllers/ExampleController.php
+++ lumen/www/app/Http/Controllers/ExampleController.php
@@ -7,9 +7,6 @@ use DB;
 class ExampleController extends Controller {
 
     public function index () {
-//        var_dump($_SERVER);
-        $result = DB::select('select 1;');
-        var_dump($result);
         return "";
     }
 
